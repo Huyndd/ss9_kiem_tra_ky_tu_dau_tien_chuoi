07ab8eabb3a9dae5c0b357d0cef8b386b9e3b783
huyndd <nongduchuy1995@gmail.com>
2019-01-16 22:26:47 +0700

    Kiểm tra ký tự đầu tiên của chuỗi có phải là chữ hoa không.

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-function isFirstLetterUpperCase(str){
+var regexp = /^[A-Z]/;
+
+function isFirstLetterUpperCase(str) {
     regexp = /^[A-Z]/;
     if (regexp.test(str)) {
         console.log("String's first character is uppercase");
     } else {
         console.log("String's first character is not uppercase");
     }
-}
+}g
